c8740c151ae98376f9b5302d236bf8897f3deb81
Diego Dias <diegodias1990@hotmail.com>
2019-06-25 12:12:47 -0300

    Nova Funcionalidade Adicionada

--- TrabalhoConclusaoGithub/Program.cs
+++ TrabalhoConclusaoGithub/Program.cs
@@ -11,6 +11,7 @@ namespace TrabalhoConclusaoGithub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git:)");
+            Console.WriteLine("Diego Dias de Matos");
         }
     }
 }
